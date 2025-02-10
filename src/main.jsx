cb5db9801e311bbd42b177d2198288231b9410e1
Pawan <tiger.pawan93@gmail.com>
2025-02-10 21:58:26 +0530

    api conflict resolve

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import HeroSection from "./Componets/HeroSection.jsx"
 import LocationProvider from "./locationContext.jsx"
+import useFetch from './Componets/useFetch.jsx'
 
 createRoot(document.getElementById('root')).render(
+  
   <LocationProvider>
     <HeroSection />
   </LocationProvider>
